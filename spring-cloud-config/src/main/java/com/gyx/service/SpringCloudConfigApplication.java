8c55d2658aea53263eda8f5a03cd229cb3a9d79b
unknown <gyxhp@foxmail.com>
2018-07-09 23:51:04 +0800

    增加配置中心模块，未配通

---
+++ spring-cloud-config/src/main/java/com/gyx/service/SpringCloudConfigApplication.java
@@ -0,0 +1,15 @@
+package com.gyx.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by gyxhp on 2018/7/9.
+ */
+@SpringBootApplication
+public class SpringCloudConfigApplication {
+    public static  void main(String[] args){
+        SpringApplication.run(SpringCloudConfigApplication.class,args);
+    }
+}
